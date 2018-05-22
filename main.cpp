2de068ebb87d7a3c6aa25807fe4f39f705425543
agui197 <33585691+agui197@users.noreply.github.com>
2018-05-22 12:29:56 -0500

    Main

---
+++ main.cpp
@@ -0,0 +1,34 @@
+//
+//  main.cpp
+//  Ejercicio_encriptacion
+//
+//  Created by Mac User on 22/05/18.
+//  Copyright © 2018 Mac User. All rights reserved.
+//
+
+#include <iostream>
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include <time.h>
+
+int main(void){
+    std::string  c;
+    std::getline(std::cin,c);
+    int i,n;
+    n=strlen(c)
+    for(i=0;i<n;i++);
+    std::cout<<c;
+}
+
+int strlen(char cadena[])
+{
+    int i,ac;
+    for(i=0,ac=0;cadena[i]!='\0';i++)
+        ac++;
+    return ac;
+}
+
+
+
+
